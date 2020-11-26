410da0a88f946d7a3447a27f8d0171b0d9d7372d
Ivander Carlos Dias <ivandercarlos@gmail.com>
2020-11-26 00:11:32 -0300

    ADD classe "active" na aba clicada

--- scripts.js
+++ scripts.js
@@ -3,3 +3,17 @@
 ///////////////////
 // Tabs
 
+const tabs = document.querySelectorAll(".tab__button");
+const tabsNav = document.querySelector(".tab__nav");
+const tabsContent = document.querySelectorAll(".tab__content");
+
+tabsNav.addEventListener("click", function(e) {
+    const tabButtonClicked = e.target.closest(".tab__button");
+    //console.log(tabButtonClicked);
+    if (!tabButtonClicked) return;
+
+    tabs.forEach(function(tab) {
+        tab.classList.remove("tab__button--active");
+    })
+    tabButtonClicked.classList.add("tab__button--active");
+});
